1be3817bf2672914daa71ad929fd552fade68f61
unknown <nildo.777@gmail.com>
2018-02-06 13:39:34 -0200

    modificada tela de estabelecimento

--- src/pages/estabelecimento/estabelecimento.ts
+++ src/pages/estabelecimento/estabelecimento.ts
@@ -16,15 +16,37 @@ export class EstabelecimentoPage {
     public navParams: NavParams,
     public platform: Platform
   ) {
+
     this.estabelecimento = this.navParams.get('estabelecimento');
-    if(this.estabelecimento)
+    console.log(this.estabelecimento);
+    if(this.estabelecimento){
       this.estabelecimento.avatar? this.photo = this.estabelecimento.avatar : this.photo = 'http://www.guiachef.com.br/wp-content/uploads/2015/06/padaria.jpg';
-    else 
-      this.navCtrl.setRoot(HomePage);
-    if(this.estabelecimento.localizacao.coord){
-      let latLng = this.estabelecimento.localizacao.coord.lat + ','+ this.estabelecimento.localizacao.coord.lng;
-      this.linkLocalizacao = this.platform.is('cordova')? 'geo:0,0?q=' + latLng + '(' + this.estabelecimento.nome + ')': "https://www.google.com.br/maps/@"+latLng +",15z?hl=pt-BR";
+      if(this.estabelecimento.localizacao.coord){
+        let latLng = this.estabelecimento.localizacao.coord.lat + ','+ this.estabelecimento.localizacao.coord.lng;
+        this.linkLocalizacao = this.platform.is('cordova')? 'geo:0,0?q=' + latLng + '(' + this.estabelecimento.nome + ')': "https://www.google.com.br/maps/@"+latLng +",15z?hl=pt-BR";
+      }
     }
+    else{
+      this.estabelecimento = {
+        key: "0",
+        categoria: "0",
+        avatar:"https://d1jgln4w9al398.cloudfront.net/imagens/ce/logosgde/logomarca%20mar_mario_rger01.jpg",
+        localizacao: {
+          coord: {
+            lat: -9.5725635,
+            lng: -35.7243071
+          },
+          logradouro: "Avenida Central, 45"
+        },
+        nome: "Marios Burgers",
+        telefone: "9999-8888",
+        telefonePrimario: "99874-5631",
+        telefoneSecundario: "3324-5458"
+      }
+      console.log('nao tem estabelecimento')
+      //this.navCtrl.setRoot(HomePage);
+    }
+    this.photo = this.estabelecimento.avatar
   }
 
   ionViewDidLoad() {
--- src/pages/sorteios-pendentes/sorteios-pendentes.ts
+++ src/pages/sorteios-pendentes/sorteios-pendentes.ts
@@ -1,6 +1,6 @@
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 
 @IonicPage()
@@ -13,7 +13,8 @@ export class SorteiosPendentesPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public sharing: SocialSharing
+    public sharing: SocialSharing,
+    public alertCtrl: AlertController
   ) {
   }
 
@@ -23,5 +24,21 @@ export class SorteiosPendentesPage {
 
   share(){
     this.sharing.shareViaFacebookWithPasteMessageHint('testando compartilhamento', 'https://firebasestorage.googleapis.com/v0/b/tradegames-2dff6.appspot.com/o/marios.jpg?alt=media&token=1da808a6-d124-4a55-b1bf-0d8ebca3be42','', 'Cole a mensagem no campo de texto')
+  }
+
+  participar(){
+    let alert = this.alertCtrl.create({
+      title: 'Atenção',
+      message: 'Lembre-se que é necessário cumprir todos os requisitos para estar apto a participar da promoção, inclusive o compartilhamento nas redes sociais!',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('Ok clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
